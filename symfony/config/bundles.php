c14a1d24195cfd9699af13fe0a00a43508f52028
Alain Tiemblo <alain.tiemblo@blablacar.com>
2019-01-20 12:47:56 +0300

    enabling the sandbox in prod environment and use it for sending smses

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -19,5 +19,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class             => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class             => ['dev' => true],
-    Bundles\SandboxBundle\SandboxBundle::class                => ['dev' => true, 'test' => true],
+    Bundles\SandboxBundle\SandboxBundle::class                => ['all' => true],
 ];
