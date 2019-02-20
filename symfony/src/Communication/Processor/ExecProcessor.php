1b077faa888f248611f0857ee30a6be1068af916
alain tiemblo <ninsuo@gmail.com>
2019-02-20 22:09:53 +0100

    remove fork + setsid on exec processor

--- symfony/src/Communication/Processor/ExecProcessor.php
+++ symfony/src/Communication/Processor/ExecProcessor.php
@@ -16,30 +16,6 @@ class ExecProcessor implements ProcessorInterface
 
     public function process(Communication $communication)
     {
-        if (!function_exists('pcntl_fork')) {
-            $this->processCommunication($communication);
-
-            return;
-        }
-
-        switch (pcntl_fork()) {
-            case -1:
-                throw new \RuntimeException(sprintf('Could not fork() in order to process communication %d', $communication->getId()));
-                break;
-            case 0:
-                if (function_exists('posix_setsid')) {
-                    posix_setsid();
-                }
-                $this->processCommunication($communication);
-                break;
-            default:
-                $status = null;
-                pcntl_wait($status);
-        }
-    }
-
-    public function processCommunication(Communication $communication)
-    {
         $console = sprintf('%s/../bin/console', $this->kernel->getRootDir());
         $command = sprintf('%s send:communication %d', escapeshellarg($console), $communication->getId());
         exec(sprintf('%s > /dev/null 2>&1 & echo -n \$!', $command));
