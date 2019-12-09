5fa52d5fd7f0128701b4ca00bbe419df46626876
alain tiemblo <ninsuo@gmail.com>
2019-12-09 21:20:10 +0100

    update session lifetime field

---
+++ symfony/src/Migrations/Version20191209201711.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191209201711 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `sessions` CHANGE COLUMN `sess_lifetime` `sess_lifetime` BIGINT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE `sessions` CHANGE COLUMN `sess_lifetime` `sess_lifetime` MEDIUMINT NOT NULL');
+    }
+}
