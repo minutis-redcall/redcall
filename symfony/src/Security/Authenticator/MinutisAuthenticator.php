1d05a93d352ee01c1f29f2696567f5c8666ea6ac
Alain Tiemblo <alain.tiemblo@blablacar.com>
2020-01-29 15:03:21 +0100

    Minutis authentication

---
+++ symfony/src/Security/Authenticator/MinutisAuthenticator.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Security\Authenticator;
+
+use App\Entity\Volunteer;
+use App\Manager\UserInformationManager;
+use App\Manager\VolunteerManager;
+use Firebase\JWT\JWT;
+use Goutte\Client;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class MinutisAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * @var VolunteerManager
+     */
+    private $volunteerManager;
+
+    /**
+     * @var UserInformationManager
+     */
+    private $userInformationManager;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var Volunteer
+     */
+    private $volunteer;
+
+    /**
+     * @param VolunteerManager       $volunteerManager
+     * @param UserInformationManager $userInformationManager
+     * @param RouterInterface        $router
+     * @param LoggerInterface|null   $logger
+     */
+    public function __construct(VolunteerManager $volunteerManager, UserInformationManager $userInformationManager, RouterInterface $router, LoggerInterface $logger = null)
+    {
+        $this->volunteerManager = $volunteerManager;
+        $this->userInformationManager = $userInformationManager;
+        $this->router = $router;
+        $this->logger = $logger ?? new NullLogger();
+    }
+
+    public function supports(Request $request)
+    {
+        return getenv('MINUTIS_JWT_PUBLIC_KEY_URL')
+            && '/auth' === $request->getPathInfo()
+            && 'POST' === $request->getMethod();
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return $request->request->get('jwt');
+    }
+
+    public function getUser($jwt, UserProviderInterface $userProvider)
+    {
+        // Decode and verify JWT token
+        try {
+            $decoded = JWT::decode($jwt, $this->getMinutisPublicKey(), ['RS256']);
+        } catch (\Throwable $e) {
+            // Either invalid JWT, invalid algo, expired token...
+            $this->logger->warning('Unable to decode JWT', [
+                'token' => $jwt,
+                'exception' => $e->getMessage(),
+            ]);
+
+            throw new BadCredentialsException();
+        }
+
+        // Seek for a nivol in payload
+        if (!array_key_exists('nivol', $decoded)) {
+            $this->logger->warning('Nivol not given in JWT', [
+                'token' => $jwt,
+            ]);
+
+            throw new BadCredentialsException();
+        }
+
+        // Seek for a volunteer attached to that nivol
+        $nivol = $decoded['nivol'];
+        $volunteer = $this->volunteerManager->findOneByNivol($nivol);
+        if (null === $volunteer) {
+            $this->logger->warning('Nivol not associated with a volunteer', [
+                'nivol' => $nivol,
+            ]);
+
+            // Will be used by onAuthenticationFailure handler
+            $this->volunteer = $volunteer;
+
+            throw new BadCredentialsException();
+        }
+
+        // Seek for a RedCall user attached to that volunteer
+        $userInformation = $this->userInformationManager->findOneByNivol($nivol);
+        if (null === $userInformation) {
+            $this->logger->info('A volunteer without RedCall access clicked on Minutis link', [
+                'nivol' => $nivol,
+            ]);
+        }
+
+        return $userInformation->getUser();
+    }
+
+    private function getMinutisPublicKey()
+    {
+        $client = new Client();
+
+        $client->request('GET', getenv('MINUTIS_JWT_PUBLIC_KEY_URL'));
+
+        return $this->client->getResponse()->getContent();
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        if ($this->volunteer) {
+            return new RedirectResponse(
+                $this->router->generate('infos', [
+                    'nivol' => $this->volunteer->getNivol(),
+                ])
+            );
+        }
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return new RedirectResponse(
+            $this->router->generate('home')
+        );
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // The login form guard authenticator is called by default.
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
