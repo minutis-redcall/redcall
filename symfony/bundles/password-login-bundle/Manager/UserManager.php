19371ede5f92510260371ef5f328ea963d4b6bb8
Alain Tiemblo <alain.tiemblo@blablacar.com>
2020-03-18 12:19:16 +0100

    Add a command to create users from nivols

--- symfony/bundles/password-login-bundle/Manager/UserManager.php
+++ symfony/bundles/password-login-bundle/Manager/UserManager.php
@@ -14,4 +14,14 @@ class UserManager extends BaseService
     {
         return $this->getManager(User::class)->findAll();
     }
+
+    public function findByUsername(string $email): ?User
+    {
+        return $this->getManager(User::class)->findOneByUsername($email);
+    }
+
+    public function save(User $user)
+    {
+        $this->getManager(User::class)->save($user);
+    }
 }
