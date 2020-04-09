2224f7a9778a199b703c5b7c1b1d99a603e8b242
Alain Tiemblo <alain.tiemblo@blablacar.com>
2020-04-09 16:59:35 +0200

    fake calls in sandbox: fallback to sms

--- symfony/bundles/sandbox-bundle/Provider/FakeCallProvider.php
+++ symfony/bundles/sandbox-bundle/Provider/FakeCallProvider.php
@@ -2,15 +2,52 @@
 
 namespace Bundles\SandboxBundle\Provider;
 
+use App\Entity\Message;
+use App\Manager\MessageManager;
 use App\Provider\Call\CallProvider;
+use App\Services\MessageFormatter;
 
 class FakeCallProvider implements CallProvider
 {
+    /**
+     * @var MessageManager
+     */
+    private $messageManager;
+
+    /**
+     * @var FakeSmsProvider
+     */
+    private $fakeSmsProvider;
+
+    /**
+     * @var MessageFormatter
+     */
+    private $formatter;
+
+    /**
+     * @param MessageManager   $messageManager
+     * @param FakeSmsProvider  $fakeSmsProvider
+     * @param MessageFormatter $formatter
+     */
+    public function __construct(MessageManager $messageManager, FakeSmsProvider $fakeSmsProvider, MessageFormatter $formatter)
+    {
+        $this->messageManager = $messageManager;
+        $this->fakeSmsProvider = $fakeSmsProvider;
+        $this->formatter = $formatter;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function send(string $phoneNumber, array $context = []): ?string
     {
-        // TODO: Implement send() method.
+        /** @var Message $message */
+        $message = $this->messageManager->find($context['message_id']);
+
+        $body = $this->formatter->formatSMSContent($message);
+
+        $this->fakeSmsProvider->send($phoneNumber, $body);
+
+        return 'ok';
     }
 }
