801e22e34da7f66caf4d47d2e8b7c247bf9b7b72
Alain Tiemblo <alain.tiemblo@blablacar.com>
2020-10-11 09:24:00 +0200

    add uri in slack context processoor

--- symfony/src/Logger/ContextProcessor.php
+++ symfony/src/Logger/ContextProcessor.php
@@ -2,6 +2,7 @@
 
 namespace App\Logger;
 
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -19,19 +20,24 @@ class ContextProcessor
     private $tokenStorage;
 
     /**
-     * @param KernelInterface       $kernel
-     * @param TokenStorageInterface $tokenStorageInterface
+     * @var RequestStack
      */
-    public function __construct(KernelInterface $kernel, TokenStorageInterface $tokenStorageInterface)
+    private $requestStack;
+
+    public function __construct(KernelInterface $kernel, TokenStorageInterface $tokenStorage, RequestStack $requestStack)
     {
         $this->kernel = $kernel;
-        $this->tokenStorage = $tokenStorageInterface;
+        $this->tokenStorage = $tokenStorage;
+        $this->requestStack = $requestStack;
     }
 
     public function __invoke(array $record)
     {
         $record['extra']['env'] = $this->kernel->getEnvironment();
         $record['extra']['platform'] = php_sapi_name();
+        if ($request = $this->requestStack->getMasterRequest()) {
+            $record['extra']['uri'] = $this->requestStack->getMasterRequest()->getUri();
+        }
 
         if ($this->tokenStorage->getToken() && $this->tokenStorage->getToken()->getUser() instanceof UserInterface) {
             $record['extra']['user'] = $this->tokenStorage->getToken()->getUser()->getUsername();
