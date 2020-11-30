fadae169fe50005ae7c1d7eb835848f388fabf38
alain tiemblo <ninsuo@gmail.com>
2020-11-30 12:25:51 +0100

    fix pegass timeout (30 secs instead of 5 mins)

--- symfony/bundles/pegass-crawler-bundle/Service/PegassClient.php
+++ symfony/bundles/pegass-crawler-bundle/Service/PegassClient.php
@@ -146,7 +146,7 @@ class PegassClient
         if (!$this->client) {
             $this->client = new Client(HttpClient::create([
                 'max_redirects' => 10,
-                'timeout'       => 300,
+                'timeout'       => 30,
             ]));
         }
 
