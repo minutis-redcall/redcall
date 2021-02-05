ce23c2217cb6441301d75320314344c95c8aa21c
alain tiemblo <ninsuo@gmail.com>
2021-02-05 23:05:54 +0100

    api documentation: constraint reader

--- symfony/bundles/api-bundle/Fetcher/PropertyFetcher.php
+++ symfony/bundles/api-bundle/Fetcher/PropertyFetcher.php
@@ -6,6 +6,7 @@ use Bundles\ApiBundle\Model\Documentation\PropertyDescription;
 use Bundles\ApiBundle\Model\Documentation\TypeDescription;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Symfony\Component\PropertyInfo\PropertyInfoExtractorInterface;
+use Symfony\Component\Validator\Constraint;
 
 class PropertyFetcher
 {
@@ -13,6 +14,11 @@ class PropertyFetcher
      * @var DocblockFetcher
      */
     private $docblockFetcher;
+
+    /**
+     * @var ConstraintFetcher
+     */
+    private $constraintFetcher;
 
     /**
      * @var PropertyInfoExtractorInterface
@@ -25,12 +31,14 @@ class PropertyFetcher
     private $annotationReader;
 
     public function __construct(DocblockFetcher $docblockFetcher,
+        ConstraintFetcher $constraintFetcher,
         PropertyInfoExtractorInterface $extractor,
         AnnotationReader $annotationReader)
     {
-        $this->docblockFetcher  = $docblockFetcher;
-        $this->extractor        = $extractor;
-        $this->annotationReader = $annotationReader;
+        $this->docblockFetcher   = $docblockFetcher;
+        $this->constraintFetcher = $constraintFetcher;
+        $this->extractor         = $extractor;
+        $this->annotationReader  = $annotationReader;
     }
 
     public function fetch(string $class, string $propertyName, ?PropertyDescription $parent) : PropertyDescription
@@ -50,12 +58,21 @@ class PropertyFetcher
 
         $property->setTitle($docblock->getSummary());
         $property->setDescription($docblock->getDescription());
+        if ('type' === $propertyName) {
+            dd($docblock);
+        }
 
         if ($parent) {
             $property->setParent($parent);
         }
 
-        // TODO constraints
+        foreach ($annotations as $annotation) {
+            if ($annotation instanceof Constraint) {
+                $property->addConstraint(
+                    $this->constraintFetcher->fetch($annotation)
+                );
+            }
+        }
 
         return $property;
     }
