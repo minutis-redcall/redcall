ec8d17dda724ceba377130e28da3239bb4a19163
alain tiemblo <ninsuo@gmail.com>
2021-02-10 00:01:44 +0100

    fix typo

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -38,17 +38,17 @@ cp deploy/${ENV}/cron.yaml symfony/
 cd symfony
 source .env >/dev/null
 
-#GREENLIGHT=$(wget -O- ${WEBSITE_URL}/deploy)
-#if [[ "${GREENLIGHT}" != "0" ]]; then
-#  echo "A communication has recently been triggered, cannot deploy before ${GREENLIGHT} seconds"
-#  cd ..
-#  cp deploying/.env symfony/.env
-#  cp deploying/google-service-account.json symfony/config/keys/google-service-account.json
-#  rm -r deploying
-#  rm symfony/app.yaml
-#  rm symfony/cron.yaml
-#  exit 1
-#fi
+GREENLIGHT=$(wget -O- ${WEBSITE_URL}/deploy)
+if [[ "${GREENLIGHT}" != "0" ]]; then
+  echo "A communication has recently been triggered, cannot deploy before ${GREENLIGHT} seconds"
+  cd ..
+  cp deploying/.env symfony/.env
+  cp deploying/google-service-account.json symfony/config/keys/google-service-account.json
+  rm -r deploying
+  rm symfony/app.yaml
+  rm symfony/cron.yaml
+  exit 1
+fi
 
 gcloud config set project ${GCP_PROJECT_NAME}
 gcloud config set app/cloud_build_timeout 3600
