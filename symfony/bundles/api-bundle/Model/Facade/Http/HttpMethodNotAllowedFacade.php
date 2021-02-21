72f7b4d7b455007dc18b39eb4f2f752192e689c3
alain tiemblo <ninsuo@gmail.com>
2021-02-21 09:04:46 +0100

    add http responses facade

---
+++ symfony/bundles/api-bundle/Model/Facade/Http/HttpMethodNotAllowedFacade.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bundles\ApiBundle\Model\Facade\Http;
+
+use Bundles\ApiBundle\Annotation as Api;
+use Bundles\ApiBundle\Model\Facade\EmptyFacade;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Api\StatusCode(Response::HTTP_METHOD_NOT_ALLOWED)
+ */
+class HttpMethodNotAllowedFacade extends EmptyFacade
+{
+}
