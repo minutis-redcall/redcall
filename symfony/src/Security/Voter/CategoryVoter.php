457edccfde36c3a543d8581cf0601a916c981bf4
alain tiemblo <ninsuo@gmail.com>
2021-03-05 09:01:46 +0100

    api: improve the doc (role without subject, no more curls)

---
+++ symfony/src/Security/Voter/CategoryVoter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Structure;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class StructureVoter extends Voter
+{
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        if (!$subject instanceof Structure) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        /** @var User $user */
+        $user = $this->security->getUser();
+        if (!$user || !($user instanceof UserInterface)) {
+            return false;
+        }
+
+        /** @var Structure $structure */
+        $structure = $subject;
+        if (0 === $structure->getIdentifier()) {
+            return false;
+        }
+
+        return $user->getStructures()->contains($structure);
+    }
+}
