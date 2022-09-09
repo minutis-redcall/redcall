098bf50bcbd82f3400ce8861fd9f9c4294cbc75c
alain tiemblo <alain@fuz.org>
2022-09-09 15:48:43 +0200

    [Operation] add an help message to know when will be configurable the operation

--- symfony/src/Form/Type/CampaignType.php
+++ symfony/src/Form/Type/CampaignType.php
@@ -5,9 +5,7 @@ namespace App\Form\Type;
 use App\Enum\Type;
 use App\Form\Model\Campaign as CampaignModel;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -27,23 +25,10 @@ class CampaignType extends AbstractType
             ->add('trigger', $options['type']->getFormType(), [
                 'label' => false,
             ])
-            ->add('notes', TextareaType::class, [
-                'label'    => 'form.campaign.fields.notes',
-                'required' => false,
-            ])
             ->add('hasOperation', CheckboxType::class, [
                 'label'    => 'form.campaign.fields.operation',
                 'required' => false,
             ]);
-
-        $builder->get('notes')->addModelTransformer(new CallbackTransformer(
-            function (?string $fromData) {
-                return $fromData;
-            },
-            function (?string $fromForm) {
-                return $fromForm ? strip_tags($fromForm) : null;
-            }
-        ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
