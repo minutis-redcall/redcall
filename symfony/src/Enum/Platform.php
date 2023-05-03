dd03312bb9fd8b150b060144a37f5ffe78f8ca6e
alain tiemblo <alain@fuz.org>
2023-05-03 17:14:45 +0200

    [Portugal] create the portugese platform

--- symfony/src/Enum/Platform.php
+++ symfony/src/Enum/Platform.php
@@ -7,9 +7,11 @@ use MyCLabs\Enum\Enum;
 /**
  * @method static $this FR
  * @method static $this ES
+ * @method static $this PT
  */
 class Platform extends Enum
 {
     public const FR = 'fr';
-    public const ES = 'es';
+//    public const ES = 'es';
+    public const PT = 'pt';
 }
